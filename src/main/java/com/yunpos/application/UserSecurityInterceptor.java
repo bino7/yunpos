5a515dace4f14870053877500455c618bbcb4461
king201301 <517420054@qq.com>
2015-07-19 14:45:26 +0800

    数据权限

--- src/main/java/com/yunpos/application/UserSecurityInterceptor.java
+++ src/main/java/com/yunpos/application/UserSecurityInterceptor.java
@@ -3,28 +3,42 @@ package com.yunpos.application;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.yunpos.model.DataRule;
 import com.yunpos.model.User;
+import com.yunpos.service.DataRuleService;
 /**
  * 拦截未登录的用户信息
  * @author kingbox
- * 2014-6-10下午9:57:20
  */
 public class UserSecurityInterceptor implements HandlerInterceptor {
+	
+	 private static final ThreadLocal<DataRule> DR = new ThreadLocal<DataRule>();
+	
+	@Autowired
+	private DataRuleService dataRuleService;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
 		 //验证用户是否登陆
         Object obj = request.getSession().getAttribute("cur_user");
-        if (obj == null || !(obj instanceof User)) {
+        String userid = request.getParameter("userid");
+        String datatype = request.getParameter("datatype");
+        if (obj != null && obj instanceof User) {
             response.sendRedirect(request.getContextPath() + "/index");
-            return false;
+            return true;
+        }else{
+        	DataRule dr =dataRuleService.findByUserID(Integer.parseInt(userid), datatype);
+        	if(dr!=null && !"".equals(dr)){
+        		DR.set(dr);
+        	}
         }
         
-        return true;
+        return false;
 	}
 
 	@Override
--- src/main/java/com/yunpos/persistence/dao/DataRuleMapper.java
+++ src/main/java/com/yunpos/persistence/dao/DataRuleMapper.java
@@ -1,19 +1,29 @@
 package com.yunpos.persistence.dao;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.yunpos.model.DataRule;
 
 public interface DataRuleMapper {
-    int deleteByPrimaryKey(Integer ruleid);
+	int deleteByPrimaryKey(Integer ruleid);
 
-    int insert(DataRule record);
+	int insert(DataRule record);
 
-    int insertSelective(DataRule record);
+	int insertSelective(DataRule record);
 
-    DataRule selectByPrimaryKey(Integer ruleid);
+	DataRule selectByPrimaryKey(Integer ruleid);
 
-    int updateByPrimaryKeySelective(DataRule record);
+	int updateByPrimaryKeySelective(DataRule record);
 
-    int updateByPrimaryKeyWithBLOBs(DataRule record);
+	int updateByPrimaryKeyWithBLOBs(DataRule record);
 
-    int updateByPrimaryKey(DataRule record);
+	int updateByPrimaryKey(DataRule record);
+
+	// 查询角色对应的数据权限
+	@Select("select * from Data_Rule where  roleid = #{roleid}")
+	DataRule selectByRoleID(Integer roleid);
+
+	// 查询用户对应的数据权限
+	@Select("select * from Data_Rule dr , User_Role ur where ur.userid = #{userid} and dr.roleid = ur.roleid and datatype= #{datatype}")
+	DataRule selectByUserID(Integer userid, String datatype);
 }

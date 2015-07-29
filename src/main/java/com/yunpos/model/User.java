3cd976fc90ee04b423a8118ca4eacc0216c2bc7d
DevinYang <375455761@qq.com>
2015-07-29 23:10:12 +0800

    解决页面sessionManage访问抛错

--- src/main/java/com/yunpos/model/User.java
+++ src/main/java/com/yunpos/model/User.java
@@ -2,6 +2,9 @@ package com.yunpos.model;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.yunpos.utils.serializer.JsonDateSerializer;
+
 public class User {
 	public static final String STATUS_DISABLED = "disabled";
 	public static final String STATUS_ENABLED = "enabled";
@@ -136,7 +139,8 @@ public class User {
 	public void setSource(String source) {
 		this.source = source == null ? null : source.trim();
 	}
-
+	
+	@JsonSerialize(using=JsonDateSerializer.class)
 	public Date getLastLoginDatetime() {
 		return lastLoginDatetime;
 	}

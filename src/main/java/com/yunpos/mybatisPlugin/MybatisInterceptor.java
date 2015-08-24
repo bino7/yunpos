8ad73bb5d3ff9719fb05fdbaedb2ecd0b3e8316c
bino7 <bino.zh@gmail.com>
2015-08-24 10:11:39 +0800

    correct it

--- src/main/java/com/yunpos/mybatisPlugin/MybatisInterceptor.java
+++ src/main/java/com/yunpos/mybatisPlugin/MybatisInterceptor.java
@@ -1,3 +1,17 @@
+/*
+ * *
+ *  * 功能描述：
+ *  * <p>
+ *  * 版权所有：小牛信息科技有限公司
+ *  * <p>
+ *  * 未经本公司许可，不得以任何方式复制或使用本程序任何部分
+ *  *
+ *  * @author Bino Zhong 新增日期：${date}
+ *  * @author Bino Zhong 修改日期：${date}
+ *  *
+ *
+ */
+
 package com.yunpos.mybatisPlugin;
 
 import java.sql.Connection;

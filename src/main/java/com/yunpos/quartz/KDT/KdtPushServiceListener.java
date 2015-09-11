d29d3eb1d7dc5678061a89aa1b0691812fbdbdab
janice <janice@janice-PC>
2015-09-11 18:52:32 +0800

    屏蔽有赞推送服务

--- src/main/java/com/yunpos/quartz/KDT/KdtPushServiceListener.java
+++ src/main/java/com/yunpos/quartz/KDT/KdtPushServiceListener.java
@@ -19,7 +19,7 @@ public class KdtPushServiceListener implements ApplicationListener<ContextRefres
 		if (context.getParent() ==null) {//防止重复执行		
 			KdtPushService kdtPushService = (KdtPushService)context.getBean("kdtPushService");
 			try {
-				kdtPushService.pushAndUpdate();
+				//kdtPushService.pushAndUpdate();
 				log.info("-------有赞推送服务KdtPushService已启动--------");
 			} catch (Exception e) {
 				log.info("-------有赞推送服务KdtPushService启动出错--------");

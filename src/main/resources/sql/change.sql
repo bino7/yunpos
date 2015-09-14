6a000bd69567b577539cd04285aef53e7c14910b
DevinYang <375455761@qq.com>
2015-09-14 17:57:27 +0800

    添加数据库变动信息

---
+++ src/main/resources/sql/change.sql
@@ -0,0 +1,9 @@
+
+--20150914 杨学勇  新增字段
+ALTER TABLE `sys_alipay_config`
+ADD COLUMN `merchanSynNotify`  varchar(255) NULL COMMENT '支付宝wap第三方同步回调地址' AFTER `appPublicKey`,
+ADD COLUMN `merchanAsynNotify`  varchar(255) NULL COMMENT '支付宝wap第三方异步回调地址' AFTER `merchanSynNotify`,
+ADD COLUMN `synNotify`  varchar(255) NULL COMMENT '支付宝wap平台同步回调地址' AFTER `merchanAsynNotify`,
+ADD COLUMN `asynNotify`  varchar(255) NULL COMMENT '支付宝wap平台异步回调地址' AFTER `synNotify`,
+ADD COLUMN `devPublicKey`  longtext NULL COMMENT '开发者公钥（服务窗用）' AFTER `asynNotify`,
+ADD COLUMN `devGetway`  varchar(255) NULL COMMENT '开发者网关（服务窗用）' AFTER `devPublicKey`;

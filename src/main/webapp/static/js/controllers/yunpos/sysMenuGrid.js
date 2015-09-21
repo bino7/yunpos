6280d229f620b7a5aa5f8fc3314398509e2e6cc2
Devin_Yang <375455761@qq.com>
2015-09-21 17:08:22 +0800

    弹出框

--- src/main/webapp/static/js/controllers/yunpos/sysMenuGrid.js
+++ src/main/webapp/static/js/controllers/yunpos/sysMenuGrid.js
@@ -1,47 +1,40 @@
 app.controller('SysMenuController', ['$scope', '$http', '$interval', 'uiGridTreeViewConstants', function ($scope, $http, $interval, uiGridTreeViewConstants ) {
-  
+	
 	$scope.gridOptions = {
     enableSorting: true,
+    enableCellEditOnFocus:true,
     enableFiltering: true,
     showTreeExpandNoChildren: true,
     columnDefs: [
-      { name: 'name', width: '15%' },
-      { name: 'gender', width: '10%' },
-      { name: 'age', width: '10%' },
-      { name: 'company', width: '15%' },
-      { name: 'state', width: '20%' },
-      { name: 'balance', width: '20%', cellFilter: 'currency' }
-    ],
+        { name: 'menuNo',displayName:'菜单编号', width: '20%' },
+		{ name: 'menuName',displayName:'菜单名', width: '20%' },
+		{ name: 'menuUrl',displayName:'链接', width: '25%' },
+		{ name: 'id',displayName:'操作', width: '28%',enableCellEdit:false,
+		// cellTemplate:'<div><a ui-sref="app.table.menu({id:row.getProperty(col.field)})" id="{{row.getProperty(col.id)}}">修改</a> |  <a ui-sref="app.table.menu({id:row.getProperty(col.id)})" id="{{row.getProperty(col.field)}}">添加下级</a>   |   <a ui-sref="app.table.menu({id:row.getProperty(col.id)})" id="{{row.getProperty(col.field)}}">删除</a></div>'}
+	    cellTemplate:'<span ng-controller="ModalDemoCtrl"> <script type="text/ng-template" id="myModalContent.html"><div ng-include="\'tpl/modal.form.html\'"></div></script><button class="btn btn-success" ng-click="open(lg)">修改</button> <button class="btn btn-success" ng-click="open(lg)">添加下级</button> <button class="btn btn-success" ng-click="open(lg)">删除</button></span>'}
+		],
     onRegisterApi: function( gridApi ) {
       $scope.gridApi = gridApi;
-      $scope.gridApi.treeBase.on.rowExpanded($scope, function(row) {
-        if( row.entity.$$hashKey === $scope.gridOptions.data[50].$$hashKey && !$scope.nodeLoaded ) {
-          $interval(function() {
-            $scope.gridOptions.data.splice(51,0,
-              {name: 'Dynamic 1', gender: 'female', age: 53, company: 'Griddable grids', balance: 38000, $$treeLevel: 1},
-              {name: 'Dynamic 2', gender: 'male', age: 18, company: 'Griddable grids', balance: 29000, $$treeLevel: 1}
-            );
-            $scope.nodeLoaded = true;
-          }, 2000, 1);
-        }
-      });
+//      $scope.gridApi.treeBase.on.rowExpanded($scope, function(row) {
+//        if( row.entity.$$hashKey === $scope.gridOptions.data[50].$$hashKey && !$scope.nodeLoaded ) {
+//          $interval(function() {
+//            $scope.gridOptions.data.splice(51,0,
+//              {name: 'Dynamic 1', gender: 'female', age: 53, company: 'Griddable grids', balance: 38000, $$treeLevel: 1},
+//              {name: 'Dynamic 2', gender: 'male', age: 18, company: 'Griddable grids', balance: 29000, $$treeLevel: 1}
+//            );
+//            $scope.nodeLoaded = true;
+//          }, 2000, 1);
+//        }
+//      });
     }
   };
  
- $http.get('vendor/modules/ui-grid/data/500_complex.json')
+ $http.get('/ajax/menu/select')
  .success(function(data) {
-   for ( var i = 0; i < data.length; i++ ){
-     data[i].state = data[i].address.state;
-     data[i].balance = Number( data[i].balance.slice(1).replace(/,/,'') );
-   }
-   data[0].$$treeLevel = 0;
-   data[1].$$treeLevel = 1;
-   data[10].$$treeLevel = 1;
-   data[11].$$treeLevel = 1;
-   data[20].$$treeLevel = 0;
-   data[25].$$treeLevel = 1;
-   data[50].$$treeLevel = 0;
-   data[51].$$treeLevel = 0;
+	   data[0].$$treeLevel = 0;
+	   data[2].$$treeLevel = 2;
+	   data[3].$$treeLevel = 3;
+
    $scope.gridOptions.data = data;
  });
  
--- src/main/webapp/static/js/controllers/yunpos/sysOrgGrid.js
+++ src/main/webapp/static/js/controllers/yunpos/sysOrgGrid.js
@@ -2,14 +2,17 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridTreeViewConst
 	
 	$scope.gridOptions = {
     enableSorting: true,
+    enableCellEditOnFocus:true,
     enableFiltering: true,
     showTreeExpandNoChildren: true,
     columnDefs: [
 		{ name: 'orgNo',displayName:'组织机构代码', width: '20%' },
 		{ name: 'orgName',displayName:'组织机构', width: '30%' },
 		{ name: 'createDate',displayName:'创建时间', type:'date',cellFilter: 'date:"yyyy-MM-dd"',width: '20%' },
-		{ name: 'oper',displayName:'操作', width: '30%',}
-    ],
+		{ name: 'id',displayName:'操作', width: '23%',enableCellEdit:false,
+		 //cellTemplate:'<div><a ui-sref="app.table.org({id:row.getProperty(col.field)})" id="{{row.getProperty(col.id)}}">修改</a> |  <a ui-sref="app.table.org({id:row.getProperty(col.id)})" id="{{row.getProperty(col.field)}}">添加下级</a>   |   <a ui-sref="app.table.org({id:row.getProperty(col.id)})" id="{{row.getProperty(col.field)}}">删除</a></div>'}
+		cellTemplate:'<span ng-controller="ModalDemoCtrl"> <script type="text/ng-template" id="myModalContent.html"><div ng-include="\'tpl/modal.form.html\'"></div></script><button class="btn btn-success" ng-click="open(lg)">修改</button> <button class="btn btn-success" ng-click="open(lg)">添加下级</button> <button class="btn btn-success" ng-click="open(lg)">删除</button></span>'}
+		],
     onRegisterApi: function( gridApi ) {
       $scope.gridApi = gridApi;
 //      $scope.gridApi.treeBase.on.rowExpanded($scope, function(row) {
@@ -28,9 +31,6 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridTreeViewConst
  
  $http.get('/ajax/org/select')
  .success(function(data) {
-   for ( var i = 0; i < data.length; i++ ){
-     data[i].oper = '修改|添加下级|删除';
-   }
 	   data[0].$$treeLevel = 0;
 	   data[2].$$treeLevel = 2;
 	   data[3].$$treeLevel = 3;

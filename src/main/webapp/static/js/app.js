f69af5657706d2f0c4ee2883fda3cc40c19e616f
tiger0-_- <hbjmlh@163.com>
2015-09-25 11:32:13 +0800

    代理商

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -22,17 +22,22 @@ angular.module('app', [
     'infinite-scroll'
 ]);
 var formatDateTime = function (date) {  
-    var y = date.getFullYear();  
-    var m = date.getMonth() + 1;  
-    m = m < 10 ? ('0' + m) : m;  
-    var d = date.getDate();  
-    d = d < 10 ? ('0' + d) : d;  
-    var h = date.getHours(); 
-    h = h < 10 ? ('0' + h) : h;  
-    var minute = date.getMinutes();  
-    minute = minute < 10 ? ('0' + minute) : minute; 
-    var s = date.getSeconds();  
-    s = s < 10 ? ('0' + s) : s; 
-    return y + '-' + m + '-' + d+' '+h+':'+minute + ":" + s;  
+	if(date instanceof Date){
+	    var y = date.getFullYear();  
+	    var m = date.getMonth() + 1;  
+	    m = m < 10 ? ('0' + m) : m;  
+	    var d = date.getDate();  
+	    d = d < 10 ? ('0' + d) : d;  
+	    var h = date.getHours(); 
+	    h = h < 10 ? ('0' + h) : h;  
+	    var minute = date.getMinutes();  
+	    minute = minute < 10 ? ('0' + minute) : minute; 
+	    var s = date.getSeconds();  
+	    s = s < 10 ? ('0' + s) : s; 
+	    return y + '-' + m + '-' + d+' '+h+':'+minute + ":" + s;  
+	}else {
+		return date;
+	}
+	
 };  
 //var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.edit', 'ui.grid.cellNav', 'addressFormatter']);
